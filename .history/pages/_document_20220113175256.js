0473935b2b68623629aef721e639e93b97011f65
Hope Kumordzie <officialhopek42@gmail.com>
2022-01-13 17:56:06 -0600

    fouc fix

--- .history/pages/_document_20220113175256.js
+++ .history/pages/_document_20220113175256.js
@@ -23,7 +23,7 @@ class MyDocument extends Document {
                     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossOrigin="anonymous"></script>
 
                 </body>
-                <script>0</script>
+                
             </Html>
         )
     }
