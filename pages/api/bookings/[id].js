abda95296679a227526553f670f53df82a347119
hope-k <hopekumordzie@gmail.com>
2022-07-18 09:37:54 +0800

    fixed auth

--- pages/api/bookings/[id].js
+++ pages/api/bookings/[id].js
@@ -3,7 +3,8 @@ import nc from 'next-connect';
 import onError from '../../../backend/middlewares/errors';
 import { getMyBookingsDetail } from '../../../backend/controllers/bookingControllers';
 import { isAuthenticatedUser } from '../../../backend/middlewares/auth';
-const handler = nc({ onError });
+const handler = nc({ onError })
+
 dbConnect();
 
 
